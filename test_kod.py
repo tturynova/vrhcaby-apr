d3f199735a1fa7c9a016433ce4df519b5c9db511
tturynovaL <teeturynova@gmail.com>
2023-05-08 20:28:28 +0200

    zkouska2

---
+++ test_kod.py
@@ -0,0 +1,27 @@
+class Hra:
+    ...
+class HerniPole:
+    def __init__(self):
+        self.bily_pole = []
+        self.cerne_pole = []
+    #aaaaaaa
+        
+class Dvojkostka:
+    ...
+class Bar:
+    ...
+
+class HerniKamen:
+      def __init__(self, pozice=-1, barva=''):
+        self.pozice = pozice
+        self.barva = barva
+        
+class Hrac:
+    ...
+
+    #aa
+
+class Hrac:
+    ...
+
+    #aa
