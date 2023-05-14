2a69db55823d89539f954d1710d8c966b58c43c3
tturynovaL <teeturynova@gmail.com>
2023-05-14 21:35:26 +0200

    trida hra

--- test_kod.py
+++ test_kod.py
@@ -1,5 +1,28 @@
 class Hra:
-    ...
+    def __init__(self, hrac1, hrac2):
+        self.hrac1 = hrac1
+        self.hrac2 = hrac2
+        self.herni_pole = HerniPole()
+        self.dvojkostka = Dvojkostka()
+        self.bar = Bar()
+        self.konec_hry = False
+        self.vitez = None
+
+    def hraj(self):
+        while not self.konec_hry:
+            tah_hrace1 = self.hrac1.tahni(self.herni_pole, self.dvojkostka, self.bar)
+            self.herni_pole.proved_tah(tah_hrace1)
+            if self.herni_pole.je_konec_hry():
+                self.konec_hry = True
+                self.vitez = self.hrac1
+                break
+            tah_hrace2 = self.hrac2.tahni(self.herni_pole, self.dvojkostka, self.bar)
+            self.herni_pole.proved_tah(tah_hrace2)
+            if self.herni_pole.je_konec_hry():
+                self.konec_hry = True
+                self.vitez = self.hrac2
+                break
+
 class HerniPole:
     def __init__(self):
         self.bily_pole = []
